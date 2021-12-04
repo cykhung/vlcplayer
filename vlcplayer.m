41244687456ea0a953133d20e41da348d1028cef
cykhung <45437973+cykhung@users.noreply.github.com>
2021-12-04 07:32:38 -0500

    Add double quotes to surround exe.

--- vlcplayer.m
+++ vlcplayer.m
@@ -2,15 +2,12 @@ function vlcplayer(filenames)
 
 exe = fullfile(fileparts(mfilename('fullpath')), 'private', ...
     'vlcplayer-3.0.7.1', 'vlc.exe');
-exe = [exe, ' '];
 
 if nargin == 0
     winopen(exe)
 else
     filenames = convert_filenames(filenames);
-    cmd = [exe, ' '];
-    cmd = [cmd, sprintf('"%s" ', filenames{:})];
-    cmd = [cmd, sprintf('&')];
+    cmd = sprintf('"%s" "%s" &', exe, filenames{:});
     dos(cmd);
 end
 
